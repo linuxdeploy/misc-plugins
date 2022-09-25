106e0a76113c294edf9748cf8fec82f9ca965988
TheAssassin <theassassin@assassinate-you.net>
2022-09-26 00:56:47 +0200

    Fix logging of grantlee5 plugins directory

--- grantlee5/linuxdeploy-plugin-grantlee5.sh
+++ grantlee5/linuxdeploy-plugin-grantlee5.sh
@@ -91,7 +91,7 @@ EOF
     popd &>/dev/null
 fi
 
-echo "GRANTLEE5_PLUGINS_DIR=$\GRANTLEE5_PLUGINS_DIR"
+echo "\$GRANTLEE5_PLUGINS_DIR=$GRANTLEE5_PLUGINS_DIR"
 
 if [[ ! -d "$GRANTLEE5_PLUGINS_DIR" ]]; then
     echo "Error: grantlee5 plugins directory does not exist or could not be found"
